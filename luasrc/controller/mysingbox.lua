c935ff8c46194a74fae656b69fe85a069a1a784d
Brand <105111832+Skycnhe@users.noreply.github.com>
2025-11-24 00:11:53 +0800

    Update mysingbox.lua

--- luasrc/controller/mysingbox.lua
+++ luasrc/controller/mysingbox.lua
@@ -1,1 +1,11 @@
 
+module("luci.controller.mysingbox", package.seeall)
+
+function index()
+	if not nixio.fs.access("/etc/config/mysingbox") then
+		return
+	end
+	
+	-- 在 "服务" 菜单下创建一个名为 "My SingBox" 的入口
+	entry({"admin", "services", "mysingbox"}, cbi("mysingbox"), _("My SingBox"), 100).dependent = true
+end
